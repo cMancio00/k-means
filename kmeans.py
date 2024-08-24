67c7d44f22d0691302d52d1292d39b4bd0c6cc2a
Christian Mancini <mancinichristian00@gmail.com>
2024-08-24 15:54:13 +0200

    Added k-means with numba and joblib

---
+++ kmeans.py
@@ -0,0 +1,81 @@
+from joblib import Parallel, delayed
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+from numba import njit
+sns.set_theme(style="whitegrid")
+
+def generate_random_points(n, dim):
+    return np.random.rand(n, dim).astype(np.float32)
+
+@njit
+def compute_label_numba(point, centroids):
+    k = centroids.shape[0]
+    distances = np.empty(k)
+    for i in range(k):
+        distances[i] = np.linalg.norm(point - centroids[i])
+    return np.argmin(distances)
+
+def compute_label_joblib(point, centroids):
+    k = centroids.shape[0]
+    distances = np.empty(k)
+    for i in range(k):
+        distances[i] = np.linalg.norm(point - centroids[i])
+    return np.argmin(distances)
+
+class KMeans:
+    def __init__(self, points, k, use_numba=True):
+        self.points = points
+        self.k = k
+        self.centroids = np.random.rand(k, np.shape(points)[1]).astype(np.float32)
+        self.labels = np.zeros(points.shape[0], dtype=np.int32)
+        self.use_numba = use_numba
+
+    def fit(self, n_jobs = 1):
+        for _ in range(100):
+            if self.use_numba:
+                new_labels = np.empty(self.points.shape[0], dtype=np.int32)
+                for i in range(self.points.shape[0]):
+                    new_labels[i] = compute_label_numba(self.points[i], self.centroids)
+            else:
+                new_labels = Parallel(n_jobs=n_jobs)(delayed(compute_label_joblib)(self.points[i], self.centroids) for i in range(self.points.shape[0]))
+                new_labels = np.array(new_labels)
+
+            if np.array_equal(self.labels, new_labels):
+                break
+
+            self.labels = new_labels
+
+            for i in range(self.k):
+                points_in_cluster = self.points[self.labels == i]
+                if len(points_in_cluster) > 0:
+                    self.centroids[i] = np.mean(points_in_cluster, axis=0)
+
+if __name__ == "__main__":
+    np.random.seed(111)
+    sizes = [10000, 10000, 100000]
+    n_jobs = [1,2,4,-1]
+    for n in sizes:
+        print(f"====={n}====")
+        points = generate_random_points(n, 2)
+        
+        kmeans_numba = KMeans(points, k=3, use_numba=True)
+        start_time = time.time()
+        kmeans_numba.fit()
+        end_time = time.time()
+        print(f"Numba: {n} points fitted in: {end_time - start_time:.4f} seconds")
+
+        for n_job in n_jobs:
+            kmeans_joblib = KMeans(points, k=3, use_numba=False)
+            start_time = time.time()
+            kmeans_joblib.fit(n_jobs=n_job)
+            end_time = time.time()
+            print(f"Joblib_{n_job}: {n} points fitted in: {end_time - start_time:.4f} seconds")
+
+        plt.figure(figsize=(10, 8))
+        sns.scatterplot(x=points[:, 0], y=points[:, 1], hue=kmeans_joblib.labels, palette='deep', s=10)
+        sns.scatterplot(x=kmeans_joblib.centroids[:, 0], y=kmeans_joblib.centroids[:, 1], color='red', s=200, marker='X')
+        plt.title(f'K-Means Clustering for {n} Points')
+        plt.savefig(f'plots/kmeans_clustering_{n}.png')
+        plt.close()
